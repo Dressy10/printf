2a5e058cc4e055a324ab2622eab64d7de64dc391
Samuel <samueldressman20@gmail.com>
2022-10-18 20:46:35 -0700

    Team Task

---
+++ print_ptg.c
@@ -0,0 +1,14 @@
+#include "mian.h"
+#include <stdio.h>
+/**
+ * print_prg - writes the character c to stdout
+ * @buf: buffer pointer
+ * @a: input char
+ * @i: index for buffer pointer
+ * Return: On success 1.
+ **/
+int print_prg(va_list a __attribute__((unused)), char *buf, unsigned int i)
+{
+	handl_buf(buf, '%', i);
+	return (1);
+}
